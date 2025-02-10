8ad28ea7da48bedc02ae7d5d38f148bb9f010a80
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2025-02-10 20:49:23 +0530

    Update update1.js

--- update1.js
+++ update1.js
@@ -7,3 +7,8 @@ else if ($rptCode[$i]==3) {
 
 echo 'Menu4_'.$j.'=new Array("'.$rptName[$i].'", "'.$fileName[$i].'?codUsr='.$cUser.
 '&todo_type='.$todo.'&codgrp='.$cgrp.'&rptCode='.$rptCode[$i].'", "", 0,30,170);'."\n";
+
+
+
+
+echo 'Menu4_'.$cnt.'=new Array("Audit for Comments", "dbe_rep_AuditExtCreationNotifComments.php?codUsr='.$cUser.'&todo_type='.$todo.'&codgrp='.$cgrp.'&rptCode=3", "", 0,30,170);'."\n";
